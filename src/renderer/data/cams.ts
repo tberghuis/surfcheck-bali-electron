4aab59e95edfd7c74eb64faedc2020c996165e53
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-02-02 12:37:06 +1100

    .

--- src/renderer/data/cams.ts
+++ src/renderer/data/cams.ts
@@ -3,26 +3,41 @@ export interface Cam {
   url: string;
 }
 
-
 // todo load from json
 
 export const cams: Cam[] = [
   {
-    name: "kuta",
-    url: "https://cams.cdn-surfline.com/cdn-int/id-kuta/playlist.m3u8",
+    name: 'Yeh Gangga',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-yehgangga/playlist.m3u8',
   },
 
   {
-    name: "lennoxhead",
-    url: "https://cams.cdn-surfline.com/cdn-au/au-lennoxhead/playlist.m3u8",
-  },
-  {
-    name: "ballinashellybeach",
-    url: "https://cams.cdn-surfline.com/cdn-au/au-ballinashellybeach/playlist.m3u8",
-  },
-  {
-    name: "ballinalighthouse",
-    url: "https://cams.cdn-surfline.com/cdn-au/au-ballinalighthouse/playlist.m3u8",
+    name: 'Pererenan',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-perenan/playlist.m3u8',
   },
 
+  {
+    name: 'berawa',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-berawa/playlist.m3u8',
+  },
+
+  {
+    name: 'kuta',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-kuta/playlist.m3u8',
+  },
+
+  {
+    name: 'Balangan',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-balangan/playlist.m3u8',
+  },
+
+  {
+    name: 'Bingin',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-binginoverview/playlist.m3u8',
+  },
+
+  {
+    name: 'Uluwatu',
+    url: 'https://cams.cdn-surfline.com/cdn-int/id-uluwatu/playlist.m3u8',
+  },
 ];
